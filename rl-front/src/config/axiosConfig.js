bc9936308fec32ead240947d556f004c8e8770c3
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-11-24 22:37:04 -0300

    Tela de cadastro Finalizada

--- rl-front/src/config/axiosConfig.js
+++ rl-front/src/config/axiosConfig.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:8080',
     headers:{
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Content-Type': 'application/json'
     }
 });
 
--- rl-front/src/pages/cadastro/cadastro.jsx
+++ rl-front/src/pages/cadastro/cadastro.jsx
@@ -3,43 +3,91 @@ import "./cadastro.css";
 import { Link } from "react-router-dom";
 import Footer from "../../components/rodape/footer";
 
+import {toast} from 'react-toastify';
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from "../../connection/apiService";
+
 export default function Cadastro() {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    nameProduct: "",
+    description:"",
+    producer:"",
+    price:0,
+    stock:0
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+    
+    try{
+
+      const product ={
+      ...formData,
+      price: Number(formData.price),
+      stock: Number(formData.stock),
+    };
+
+      await ApiService.product.createProduct(product);
+
+      toast.success("Produto cadastrado com sucesso!");
+      navigate("/lista");
+    }
+    catch (error){
+      if(error.response && error.response.status === 400){
+        const errors = error.response.data;
+
+        Object.values(errors).forEach((msg) => toast.error(msg));
+      }
+      else{
+        toast.error("Erro ao cadastrar produto.");
+      }
+    }
+  };
+
   return (
     <>
       <article className="topo-cadastro">
         <main className="conteudo-cadastro">
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>Incluir novo produto</h1>
 
             <div>
               <label>Produto:</label>
-              <input type="text" id="nome" name="nome" />
+              <input type="text" id="nome" name="nameProduct" value={formData.nameProduct} onChange={handleChange} />
             </div>
 
             <div>
               <label>Fabricante:</label>
-              <input type="text" id="fabricante" name="fabricante" />
+              <input type="text" id="fabricante" name="producer" value={formData.producer} onChange={handleChange}  />
             </div>
 
             <div>
               <label>Preço:</label>
-              <input type="number" id="preco" name="preco" />
+              <input type="number" id="preco" name="price" value={formData.price} onChange={handleChange} />
             </div>
 
             <div>
               <label>Quantidade em estoque:</label>
-              <input type="number" id="estoque" name="estoque" />
+              <input type="number" id="estoque" name="stock" value={formData.stock}  onChange={handleChange} />
             </div>
 
             <div className="descricao">
-              <label>Descrição do produto:</label>
-              <input type="text" id="descricao" name="descricao" />
+              <label>Descrição:</label>
+              <textarea type="text" id="descricao" name="description" value={formData.description} onChange={handleChange} ></textarea>
             </div>
 
-           
-            <Link to="/lista" style={{textDecoration: "none"}}>
-              <button>Enviar</button>            
-            </Link>
+            <button type="submit">Enviar</button>            
+            
           </form>
         </main>
       </article>
